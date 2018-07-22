c23e1a166274f11f7bce898bd3269d233715a892
Renjith KR <renjit2255@gmail.com>
2018-07-22 22:18:27 +0530

    Initial commit

---
+++ src/main/java/com/cw/blog/models/User.java
@@ -0,0 +1,61 @@
+package com.cw.blog.models;
+
+import java.sql.Date;
+
+public class User {
+	private String name;
+	private String firstName;
+	private String LastName;
+	private String email;
+	private String phone;
+	private Date createdDate;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+}
